ddbf65d8eae852ed84a08b00eae6bc0cf7c44447
Craig Eaton <craigea92@gmail.com>
2023-05-11 15:44:32 +0100

    Base layout complete for pokedex app

--- src/sections/NavBar.tsx
+++ src/sections/NavBar.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import pokeballIcon from "../assets/pokeball-icon.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="pokeball icon" />
+      </div>
+      <div className="data"></div>
+      <div className="block">
+        <GiHamburgerMenu />
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
