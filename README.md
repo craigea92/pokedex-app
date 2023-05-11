d899ec0f241b5b6dcef47082b77c849583923236
Craig Eaton <craigea92@gmail.com>
2023-05-11 15:30:16 +0100

    Added SCSS files for styling and created a basic background for our app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pokedex App
+
+## SCSS
+
+Just like CSS, Scss also supports the @import directive. The @import directive allows you to include the content of one file in another.
+
+The CSS @import directive has a major drawback due to performance issues; it creates an extra HTTP request each time you call it. However, the Scss @import directive includes the file in the CSS; so no extra HTTP call is required at runtime!
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import NavBar from "./sections/NavBar";
+import Wrapper from "./sections/Wrapper";
+import Footer from "./sections/Footer";
+import Background from "./components/Background";
+import "./scss/index.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="main-container">
+      <Background />
+      <div className="app">
+        <NavBar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
