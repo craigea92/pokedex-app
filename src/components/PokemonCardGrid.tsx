4281f17c98f80f427a752c05264d50efbf7ee1de
Craig Eaton <craigea92@gmail.com>
2023-05-16 18:23:25 +0100

    Added the types information to the cards and styled them

--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { userPokemonsType } from "../utils/Types";
+import { pokemonTypeInterface, userPokemonsType } from "../utils/Types";
 
 function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
   return (
@@ -17,7 +17,29 @@ function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
                   src={data.image}
                   alt="pokemon"
                   className="pokemon-card-image"
+                  loading="lazy"
                 />
+                <div className="pokemon-card-types">
+                  {data.types.map(
+                    (type: pokemonTypeInterface, index: number) => {
+                      const keys = Object.keys(type);
+
+                      return (
+                        <div className="pokemon-card-types-type" key={index}>
+                          <img
+                            src={type[keys[0]].image}
+                            alt="pokemon type"
+                            className="pokemon-card-types-type-image"
+                            loading="lazy"
+                          />
+                          <h6 className="pokemon-card-types-type-text">
+                            {keys[0]}
+                          </h6>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
               </div>
             );
           })}
