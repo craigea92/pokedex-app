6674841be3f57a42006c4f73daf2d9ba592d01ba
Craig Eaton <craigea92@gmail.com>
2023-06-05 13:26:56 +0100

    Added comments to the getPokemonData reducer

--- src/app/reducers/getPokemonData.ts
+++ src/app/reducers/getPokemonData.ts
@@ -8,8 +8,12 @@ export const getPokemonData = createAsyncThunk(
   "pokemon/randomPokemon",
   async (pokemons: genericPokemonType[]) => {
     try {
+      // An array to store the fetched Pokemon data
       const pokemonsData: generatedPokemonType[] = [];
+
+      // Looping through each Pokemon in the input array
       for await (const pokemon of pokemons) {
+        // Fetching data for each Pokemon from the provided URL using axios
         const {
           data,
         }: {
@@ -18,12 +22,16 @@ export const getPokemonData = createAsyncThunk(
             types: { type: generatedPokemonType }[];
           };
         } = await axios.get(pokemon.url);
+
+        // Extracting types information for the Pokemon
         const types = data.types.map(
           ({ type: { name } }: { type: { name: string } }) => ({
             // @ts-expect-error
             [name]: pokemonTypes[name],
           })
         );
+
+        // Fetching the corresponding image for the Pokemon
         // @ts-expect-error
         let image: string = images[data.id];
         if (!image) {
@@ -31,6 +39,7 @@ export const getPokemonData = createAsyncThunk(
           image = defaultImages[images.id];
         }
 
+        // Adding the Pokemon's data to the pokemonsData array if an image is found
         if (image) {
           pokemonsData.push({
             name: pokemon.name,
@@ -40,8 +49,11 @@ export const getPokemonData = createAsyncThunk(
           });
         }
       }
+
+      // Returning the fetched Pokemon data
       return pokemonsData;
     } catch (err) {
+      // Logging any errors that occur during the process
       console.log(err);
     }
   }
