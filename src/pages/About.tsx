c6cffa957e8e7ac8aa3dddd9a0065556d64712cc
Craig Eaton <craigea92@gmail.com>
2023-06-06 15:36:09 +0100

    Styled the About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,17 @@ import Wrapper from "../sections/Wrapper";
 import avatarImage from "../assets/Craig.png";
 
 function About() {
-  return <div className="about">
-    <img src={avatarImage} alt="avatar"/>
-    <h1 className="profile-text">Hi, I am Craig!</h1>
-    <h2 className="profile-text">The creator of this awesome Pokedex</h2>
-    <h4 className="profile-text">I created this project because I wanted to learn more about APIs and I'm a huge fan of Pokemon.</h4>
-  </div>;
+  return (
+    <div className="profile">
+      <img src={avatarImage} alt="avatar" className="profile-image" />
+      <h1 className="profile-text">Hi, I am Craig!</h1>
+      <h2 className="profile-text">The creator of this awesome Pokedex</h2>
+      <h4 className="profile-text">
+        I created this project because I wanted to learn more about APIs and I'm
+        a huge fan of Pokemon.
+      </h4>
+    </div>
+  );
 }
 
 export default Wrapper(About);
