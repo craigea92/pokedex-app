efcd0572d781104422cb8d46c5a4610d76766773
Craig Eaton <craigea92@gmail.com>
2023-06-07 13:25:11 +0100

    Styled the about page and added routes to the Pokemon page

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,17 +4,52 @@ import { MdOutlinePowerSettingsNew } from "react-icons/md";
 import { firebaseAuth } from "../utils/FirebaseConfig";
 import { useAppDispatch } from "../app/hooks";
 import { setToast, setUserStatus } from "../app/slices/AppSlice";
+import { pokemonTabs } from "../utils/Constants";
+import { useLocation } from "react-router";
 function Footer() {
   const dispatch = useAppDispatch();
+  const location = useLocation();
   const handleLogout = () => {
     signOut(firebaseAuth);
     dispatch(setUserStatus(undefined));
-    dispatch(setToast("Logged out successfully"))
+    dispatch(setToast("Logged out successfully"));
   };
+
+  const routes = [
+    {
+      name: pokemonTabs.description,
+      value: "Description",
+    },
+    {
+      name: pokemonTabs.evolution,
+      value: "Evolution",
+    },
+    {
+      name: pokemonTabs.locations,
+      value: "Catching",
+    },
+    {
+      name: pokemonTabs.moves,
+      value: "Capable Moves",
+    },
+  ];
+
   return (
     <footer>
       <div className="block"></div>
-      <div className="data"></div>
+      <div className="data">
+        {location.pathname.includes("/pokemon") && (
+          <ul>
+            {routes.map((route) => {
+              return (
+                <li key={route.name} className="" onClick={() => {}}>
+                  {route.value}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
       <div className="block">
         <MdOutlinePowerSettingsNew onClick={handleLogout} />
       </div>
