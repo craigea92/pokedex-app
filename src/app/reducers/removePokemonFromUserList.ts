d3a37f4137a6b1c1cd954c571f0c09c41e4a8273
Craig Eaton <craigea92@gmail.com>
2023-06-11 17:46:23 +0100

    Added comments to the removePokemon reducer

--- src/app/reducers/removePokemonFromUserList.ts
+++ src/app/reducers/removePokemonFromUserList.ts
@@ -6,9 +6,13 @@ export const removePokemonFromUserList = createAsyncThunk(
   "pokemon/remove",
   async ({ id }: { id: string }) => {
     try {
+      // Deleting the Pokemon document from Firestore based on the provided id
       await deleteDoc(doc(pokemonListRef, id));
+
+      // Returning the id of the removed Pokemon
       return { id };
     } catch (err) {
+      // Logging any errors that occur during the process
       console.log(err);
     }
   }
