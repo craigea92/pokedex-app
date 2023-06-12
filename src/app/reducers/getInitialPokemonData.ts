db1397b81b7341bad75006d38d789d935ebb836d
Craig Eaton <craigea92@gmail.com>
2023-06-12 18:25:12 +0100

    Updated the getinitialpokemon reducer comments

--- src/app/reducers/getInitialPokemonData.ts
+++ src/app/reducers/getInitialPokemonData.ts
@@ -6,16 +6,14 @@ export const getInitialPokemonData = createAsyncThunk(
   "pokemon/initialData",
   async () => {
     try {
+      // Fetching initial Pokemon data from the specified route using axios
       const { data } = await axios.get(pokemonsRoute);
+
+      // Returning the 'results' property from the fetched data
       return data.results;
     } catch (err) {
+      // Logging any errors that occur during the process
       console.log(err);
     }
   }
 );
-
-/*
-The getInitialPokemonData function makes an API call using axios.get 
-to fetch data from the pokemonsRoute URL, and it returns the data.results 
-from the API response. 
-*/
