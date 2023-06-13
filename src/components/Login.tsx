2491153fa33ff2f48a954e4d0d92baf46585cf45
Craig Eaton <craigea92@gmail.com>
2023-06-13 17:32:12 +0100

    Added comments to Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,17 +8,27 @@ import { useAppDispatch } from "../app/hooks";
 
 function Login() {
   const dispatch = useAppDispatch();
+
+  // Function to handle the login process
   const handleLogin = async () => {
+    // Creating a new GoogleAuthProvider instance
     const provider = new GoogleAuthProvider();
+    // Signing in with Google using a popup window
     const {
       user: { email, uid },
     } = await signInWithPopup(firebaseAuth, provider);
+    
+    // Checking if the email is available
     if (email) {
+      // Creating a Firestore query to fetch the user based on the uid
       const firestoreQuery = query(usersRef, where("uid", "==", uid));
+      // Fetching the user data from Firestore
       const fetchedUser = await getDocs(firestoreQuery);
+      // If the user doesn't exist in Firestore, add the user to the collection
       if (fetchedUser.docs.length === 0) {
         await addDoc(usersRef, { uid, email });
       }
+      // Dispatching an action to set the user status in the app state
       dispatch(setUserStatus({ email }));
     }
   };
