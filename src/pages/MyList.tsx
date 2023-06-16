4e638171efbbda49c4811c59e19a35292f1df499
Craig Eaton <craigea92@gmail.com>
2023-06-16 20:58:12 +0100

    Added comments to MyList.tsx

--- src/pages/MyList.tsx
+++ src/pages/MyList.tsx
@@ -6,10 +6,14 @@ import PokemonCardGrid from "../components/PokemonCardGrid";
 import { getUserPokemons } from "../app/reducers/getUserPokemons";
 
 function MyList() {
+  // Extracting the userInfo data from the app state using the useAppSelector hook
   const { userInfo } = useAppSelector(({ app }) => app);
+  // Extracting the userPokemons data from the pokemon state using the useAppSelector hook
   const { userPokemons } = useAppSelector(({ pokemon }) => pokemon);
+  // Getting the dispatch function using the useAppDispatch hook
   const dispatch = useAppDispatch();
   useEffect(()=> {
+    // Dispatching the getUserPokemons action when the userInfo or dispatch dependencies change
     dispatch(getUserPokemons());
   }, [userInfo, dispatch]);
   return (
