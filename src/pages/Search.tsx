1d05d31573ff3f23364467eb045af9bdc16b1b9a
Craig Eaton <craigea92@gmail.com>
2023-06-17 21:12:56 +0100

    Added comments to Search.tsx and changed slice 20 to 50

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -7,39 +7,51 @@ import PokemonCardGrid from "../components/PokemonCardGrid";
 import { debounce } from "../utils/Debounce";
 
 function Search() {
+  // Getting the dispatch function using the useAppDispatch hook
   const dispatch = useAppDispatch();
+  // Extracting the allPokemon and randomPokemons data from the pokemon state using the useAppSelector hook
   const { allPokemon, randomPokemons } = useAppSelector(
     ({ pokemon }) => pokemon
   );
   useEffect(() => {
+    // Dispatching the getInitialPokemonData action when the component mounts
     dispatch(getInitialPokemonData());
   }, [dispatch]);
 
   useEffect(() => {
     if (allPokemon) {
+      // Cloning the allPokemon array
       const clonedPokemons = [...allPokemon];
+      // Randomly selecting 50 pokemon IDs from the clonedPokemons array
       const randomPokemonsId = clonedPokemons
         .sort(() => Math.random() - Math.random())
-        .slice(0, 20);
+        .slice(0, 50);
+      // Dispatching the getPokemonData action with the randomly selected pokemon IDs
       dispatch(getPokemonData(randomPokemonsId));
     }
   }, [allPokemon, dispatch]);
 
+  // Function to get pokemon data based on search value
   const getPokemon = async (value: string) => {
     if (value.length) {
+      // Filtering the allPokemon array based on the search value
       const pokemons = allPokemon?.filter((pokemon) =>
         pokemon.name.includes(value.toLowerCase())
       );
+      // Dispatching the getPokemonData action with the filtered pokemons
       dispatch(getPokemonData(pokemons!));
     } else {
       const clonedPokemons = [...(allPokemon as [])];
+      // Randomly selecting 50 pokemon IDs from the clonedPokemons array
       const randomPokemonsId = clonedPokemons
         .sort(() => Math.random() - Math.random())
-        .slice(0, 20);
+        .slice(0, 50);
+      // Dispatching the getPokemonData action with the randomly selected pokemon IDs
       dispatch(getPokemonData(randomPokemonsId));
     }
   };
-
+  
+  // Debounced function to handle search input change
   const handleChange = debounce((value: string) => getPokemon(value), 300);
 
   return (
