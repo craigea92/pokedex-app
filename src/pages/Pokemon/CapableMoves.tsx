59e17a69a8c609f364efc66788ead989593fc987
Craig Eaton <craigea92@gmail.com>
2023-06-24 09:02:56 +0100

    Added capable moves logicready to be styled

--- src/pages/Pokemon/CapableMoves.tsx
+++ src/pages/Pokemon/CapableMoves.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useAppSelector } from "../../app/hooks";
 
 function CapableMoves() {
-  return <div>CapableMoves</div>;
+  // Extracting the currentPokemon data from the pokemon state using the useAppSelector hook
+  const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+  );
+  return (
+    <div className="capable-moves">
+      <h1 className="capable-move-title">Abilities</h1>
+      <ul className="capable-moves-list ability">
+
+        {pokemonData?.pokemonAbilities.abilities.map((ability:string)=> (
+          <li key={ability} className="pokemon-location">
+            {ability}
+          </li>
+        ))}
+      </ul>
+      <h1 className="capable-move-title">Moves</h1>
+      <ul className="capable-moves-list ability">
+
+        {pokemonData?.pokemonAbilities.moves.map((move:string)=> (
+          <li key={move} className="pokemon-location">
+            {move}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default CapableMoves;
